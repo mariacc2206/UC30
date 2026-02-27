bd33a05e80347ed123f26bcbe5e8205849236f22
mariacc2206 <mariadino2009@gmail.com>
2026-02-26 22:43:36 -0300

    Add files via upload

---
+++ pratica.py
@@ -0,0 +1,6 @@
+distancia = float(input("Digite a distancia em metros:"))
+Combustivel = float(input("Digite a distancia em metros:"))
+
+consumo = distancia / Combustivel
+
+print("O consumo da carro é de", consumo, "Km/l")
---
+++ pratica2.py
@@ -0,0 +1,5 @@
+resp = input("Deseja continuar? (s/n): ")
+if resp.lower() == "s":
+    print("Continuando...")
+else:
+    print("Encerrando...")  
