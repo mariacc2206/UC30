168625520e53c14c72c5b20847283844e4b0d43b
mariacc2206 <mariadino2009@gmail.com>
2026-03-04 15:56:57 -0300

    Add files via upload

---
+++ UC- Algoritimo(04-03)/UC-algoritmo(04-03-26)/atividade.py
@@ -0,0 +1,7 @@
+#Crie uma lista chamada COMPRAS com pelo menos 5 itens. Adicione um novo item à lista de forma que o usuário digite
+produto = ["Sabonete", "perfumes", "hidratantes", "Body splash", "Lenços"]
+print("Lista inicial:", produto)
+resposta = input("Digite um produto: ")
+
+produtos.append("Resposta") 
+print("Lista atualizada:", produto)
