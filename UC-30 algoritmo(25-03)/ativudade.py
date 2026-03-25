7a5ad3c77b4559ce9d6ab6d492060a3dbf5fefc1
mariacc2206 <mariadino2009@gmail.com>
2026-03-25 08:32:09 -0300

    Add files via upload

---
+++ UC-30 algoritmo(25-03)/ativudade.py
@@ -0,0 +1,18 @@
+numero1 = input(int("Digite um número: "))
+numero2 = input(int("Digite um número: "))
+
+def soma(numero1, numero2):
+    resultado = numero1 + numero2
+    print(f"O resultado da soma foi {resultado}.")
+
+def subtração(numero1, numero2):
+    resultado2 = numero1 + numero2
+    print(f"O resultado da subtração foi {resultado2}")
+
+def multiplicação (numero1, numero2):
+    resultado3 = numero1 + numero2
+    print(f"O resultado da soma foi {resultado3}")
+
+def divisao (numero1, numero2):
+    resultado4 = numero1 + numero2
+    print(f"O resultado da divisão foi {resultado4}")
